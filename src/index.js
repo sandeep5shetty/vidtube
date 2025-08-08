789bfb3d0243c6e39dfe36cc4c4f155999c652b5
Sandeep Shetty <sandeepshettysigadalu@gmail.com>
2025-08-08 23:12:41 +0530

    initialize vidtube project with basic structure, dependencies and connecting with  mongoDB

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env",
+});
+
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Database connection failed:", error);
+        process.exit(1);
+    });
