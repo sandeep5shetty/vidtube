773459cbd23182289efdc580627bd29eaf9627a2
Sandeep Shetty <sandeepshettysigadalu@gmail.com>
2025-08-11 20:57:48 +0530

    add cloudinary and multer for file uploads

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import cors from 'cors';
-
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -18,6 +16,8 @@ app.use(
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
+
+app.use(cookieParser());
 
 //import routes
 import healthcheckRouter from './routes/healthcheck.routes.js';
